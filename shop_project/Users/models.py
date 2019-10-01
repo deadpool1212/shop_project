1be8280db11cf46f1cb15475ce5ee5c6c72c19f6
deadpool1212 <shahkanishk99@gmail.com>
2019-10-01 21:17:10 +0530

    Initial commit

---
+++ shop_project/Users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    phone_number = models.IntegerField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
